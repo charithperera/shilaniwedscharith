d7388b31d326f939dea2e2d01df07e62df653e7c
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2017-01-27 22:53:33 +0000

    Navbar complete

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
 
-  $(".countdown").text(getDays() + " Days");
+  $logo = $('.navbar-header .navbar-brand img');
 
-  function getDays() {
-    var now = moment(); //todays date
-    var end = moment("2017-09-09"); // another date
-    var duration = moment.duration(now.diff(end));
-    var days = duration.asDays();
-    return Math.floor(Math.abs(days));
-  }
+  $logo.mouseover(function() {
+    $(this).attr("src", 'img/scgold.png');
+  })
+
+  $logo.mouseout(function() {
+    $(this).attr("src", 'img/sc.png');
+  })
+
+  // $(".countdown").text(getDays() + " Days");
+  //
+  // function getDays() {
+  //   var now = moment(); //todays date
+  //   var end = moment("2017-09-09"); // another date
+  //   var duration = moment.duration(now.diff(end));
+  //   var days = duration.asDays();
+  //   return Math.floor(Math.abs(days));
+  // }
 })
