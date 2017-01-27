d8f59365787671db46608670c3e9a30315e6eb1d
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2017-01-27 23:41:29 +0000

    Header progress

--- js/app.js
+++ js/app.js
@@ -1,22 +1,17 @@
 $(document).ready(function() {
 
   $logo = $('.navbar-header .navbar-brand img');
+  $countdown = $(".countdown");
 
-  $logo.mouseover(function() {
-    $(this).attr("src", 'img/scgold.png');
-  })
+  $logo.mouseover(function() { $(this).attr("src", 'img/scgold.png'); })
+  $logo.mouseout(function() { $(this).attr("src", 'img/sc.png');})
+  $countdown.text(getDays() + " Days");
 
-  $logo.mouseout(function() {
-    $(this).attr("src", 'img/sc.png');
-  })
-
-  // $(".countdown").text(getDays() + " Days");
-  //
-  // function getDays() {
-  //   var now = moment(); //todays date
-  //   var end = moment("2017-09-09"); // another date
-  //   var duration = moment.duration(now.diff(end));
-  //   var days = duration.asDays();
-  //   return Math.floor(Math.abs(days));
-  // }
+  function getDays() {
+    var now = moment(); //todays date
+    var end = moment("2017-09-09"); // another date
+    var duration = moment.duration(now.diff(end));
+    var days = duration.asDays();
+    return Math.floor(Math.abs(days));
+  }
 })
