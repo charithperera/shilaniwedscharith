f06c8cd26d6efcc26f1f63255a28cad539481998
Charith Perera <charith.p@gmail.com>
2017-01-29 12:34:38 +1100

    Begin rsvp calls

--- js/app.js
+++ js/app.js
@@ -2,10 +2,35 @@ $(document).ready(function() {
 
   $logo = $('.navbar-header .navbar-brand img');
   $countdown = $(".countdown");
+  $pinBoxes = $('.pin-box');
+  $guests = $(".guests");
+  $formPin = $("#form-pin")
 
   $logo.mouseover(function() { $(this).attr("src", 'img/scgold.png'); })
   $logo.mouseout(function() { $(this).attr("src", 'img/sc.png');})
   $countdown.text(getDays() + " Days");
+  $pinBoxes.keyup(movePinCursor);
+  $formPin.submit(pinSubmission);
+
+  function pinSubmission(e) {
+    e.preventDefault();
+    $.ajax({
+      url: 'http://localhost:3000/getinvitation',
+      method: 'get'
+    })
+    .done(function(resp) {
+      debugger
+    });
+  }
+
+  function movePinCursor(e) {
+    if ($(this).val() === "") {
+      $(this).prev('.pin-box').focus();
+    }
+    else {
+      $(this).next('.pin-box').focus();
+    }
+  }
 
   function getDays() {
     var now = moment(); //todays date
